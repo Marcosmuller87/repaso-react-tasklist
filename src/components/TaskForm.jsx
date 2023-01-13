860c44501f7d428c87379419b93e91abffcdcbb7
Marcos Muller <marcos.muller@magmati.com.br>
2023-01-13 16:33:18 -0300

    primer commit

---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskForm() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const { createTask } = useContext(TaskContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask({ title, description });
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <div className="max-w-md mx-auto">
+      <form className="bg-slate-800 p-10 mb-4" onSubmit={handleSubmit}>
+        <h2 className="text-white text-2xl mb-4">Nueva Tarea</h2>
+        <input
+          className="bg-slate-300 p-2 mb-4 w-full"
+          type="text"
+          placeholder="Write a task"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          value={title}
+          autoFocus
+        />
+        <textarea
+          className="bg-slate-300 p-2 mb-4 w-full"
+          placeholder="Descripcion"
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+          value={description}
+        />
+        <button className="bg-indigo-500 px-3 py-1 text-white">Guardar</button>
+      </form>
+    </div>
+  );
+}
+
+export default TaskForm;
---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 1,
+    title: 'Doctors Appointment',
+    description: 'Meet with Dr. Smith at 3:00pm'
+  },
+  {
+    id: 2,
+    title: 'Meeting at School',
+    description: 'Discuss project with class'
+  },
+  {
+    id: 3,
+    title: 'Food Shopping',
+    description: 'Buy food for the week'
+  }
+]
